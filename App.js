166fcbf31b45db426fc5a2f6006288fcedfc0784
Amad Yaca <amadyaca@localhost.localdomain>
2019-02-24 14:27:04 -0600

    4. se agrego la libreria react-navigation

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welnotecrecome to React Native!</Text>
       </View>
     );
   }
