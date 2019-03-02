f384899b623d422f956e7bf2b35d6efbcdd8328f
Amad Yaca <amadyaca@localhost.localdomain>
2019-03-02 11:13:07 -0600

    21. se agrego un marcador personalizado

--- mapa/VistaMapa.js
+++ mapa/VistaMapa.js
@@ -1,70 +1,105 @@
 import React, { Component } from 'react';
-import MapView, {  Marker } from 'react-native-maps';
-import { StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { View, StyleSheet } from 'react-native';
 
 export class VistaMapa extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            //coordenadas de guzman, el punto inicial
-            latitude: 19.7046600,
-            longitude: -103.4617000,
-            error: null
+            latitude: 0,
+            longitude: 0,
         }
     }
 
-    //OBTIENE LA UBICACIÓN EXACTA DEL USUARIO
-    componentDidMount(){
-        navigator.geolocation.getCurrentPosition(position => {
-            this.setState({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                error: null,
-            })
-        }, error => this.setState({error: error.message}),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 2000 }
-        )
+    //cambia los valores de longitude y latitude en state por los obtenidos de la ubicacion del usuario
+    //no funciona
+    especificarCoordenadasObtenidas(position) {
+        this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        })
     }
 
+    //OBTIENE LA UBICACIÓN EXACTA DEL USUARIO
+    componentDidMount() {
+        //se obtiene la posicion
+        navigator.geolocation.getCurrentPosition(position => {
+            this.especificarCoordenadasObtenidas(position)
+            //var initialPosition = JSON.stringify(position);
+            //this.setState({ initialPosition });
+        },
+            //se maneja el error  
+            error => alert(error.message),
+            //se establecen algunas propiedades de la ubicacion
+            {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 2000
+            })
+    }
 
     render() {
+        const tuxpan_coords = {
+            latitude: 19.55498,
+            longitude: -103.37763833333332,
+            latitudeDelta: .0922,
+            longitudeDelta: .0421,
+        }
 
-        const guzman = {
-            //latitude: 19.7046600,
-            //longitude: -103.4617000,
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+        const guzman_coords = {
+            latitude: 19.7046600,
+            longitude: -103.4617000,
+            latitudeDelta: 0.030,
+            longitudeDelta: 0.030,
         }
 
         return (
             <MapView
                 style={styles.map}
-                initialRegion={guzman}
+                initialRegion={tuxpan_coords}
+                //showsUserLocation
             >
                 <Marker
                     coordinate={{
-                        latitude: this.state.latitude,
-                        longitude: this.state.longitude,
-                        latitudeDelta: 0.015,//0.0922,
-                        longitudeDelta: 0.0121,//0.0421,
+                        latitude: this.state.latitude,//19.55498,
+                        longitude: this.state.longitude,//-103.37763833333332,
                     }}
-                    pinColor="blue"
-                />
+                >
+                    <View style={styles.radius}>
+                        <View style={styles.marker} />
+                    </View>
+                </Marker>
             </MapView>
-        );
+        )
     }
 }
 
 const styles = StyleSheet.create({
     map: {
-        //flex:1,
         position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
     },
+    marker: {
+        height: 20,
+        width: 20,
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 10,
+        overflow: 'hidden',
+        backgroundColor: 'red',
+    },
+    radius: {
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(0,122,255, 0.1)',
+        borderWidth: 1,
+        borderColor: 'rgba(0,122,255, 0.3)',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
